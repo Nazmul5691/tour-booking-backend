d35da92db888db6ccceea69f639b1269349f1b62
Md Nazmul Islam <nazmulislam5691@gmail.com>
2026-01-04 17:13:37 +0600

    update gitingnore

---
+++ src/app/config/multer-storage-cloudinary.d.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'multer-storage-cloudinary' {
+  import { StorageEngine } from 'multer';
+  import { v2 as cloudinary } from 'cloudinary';
+
+  interface CloudinaryStorageOptions {
+    cloudinary: typeof cloudinary;
+    params:
+      | {
+          folder?: string;
+          format?: string;
+          public_id?: (req: Express.Request, file: Express.Multer.File) => string;
+          transformation?: object[];
+          [key: string]: any;
+        }
+      | ((
+          req: Express.Request,
+          file: Express.Multer.File
+        ) => {
+          folder?: string;
+          format?: string;
+          public_id?: string;
+          transformation?: object[];
+          [key: string]: any;
+        });
+  }
+
+  export class CloudinaryStorage implements StorageEngine {
+    constructor(options: CloudinaryStorageOptions);
+    _handleFile(
+      req: Express.Request,
+      file: Express.Multer.File,
+      callback: (error?: any, info?: Partial<Express.Multer.File>) => void
+    ): void;
+    _removeFile(
+      req: Express.Request,
+      file: Express.Multer.File,
+      callback: (error: Error | null) => void
+    ): void;
+  }
+}
--- src/app/config/multer.cofig.ts
+++ src/app/config/multer.cofig.ts
@@ -2,31 +2,67 @@ import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { cloudinaryUpload } from "./cloudinary.config";
 
-
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinaryUpload,
-    params: {
-        public_id: (req, file) => {
-            // My Special.Image#!@.png => 4545adsfsadf-45324263452-my-image.png
-            // My Special.Image#!@.png => [My Special, Image#!@, png]
+  cloudinary: cloudinaryUpload,
+  params: {
+    public_id: (_req, file) => {
+      // Prefix with underscore since req is unused
+      const fileName = file.originalname
+        .toLowerCase()
+        .replace(/\s+/g, "-")
+        .replace(/\./g, "-")
+        // eslint-disable-next-line no-useless-escape
+        .replace(/[^a-z0-9\-\.]/g, "");
 
-            const fileName = file.originalname
-                .toLowerCase()
-                .replace(/\s+/g, "-") // empty space remove replace with dash
-                .replace(/\./g, "-")
-                // eslint-disable-next-line no-useless-escape
-                .replace(/[^a-z0-9\-\.]/g, "") // non alpha numeric - !@#$
+      const uniqueFileName =
+        Math.random().toString(36).substring(2) +
+        "-" +
+        Date.now() +
+        "-" +
+        fileName;
 
-            // const extension = file.originalname.split(".").pop()
+      return uniqueFileName;
+    },
+  },
+});
 
-            // binary -> 0,1 hexa decimal -> 0-9 A-F base 36 -> 0-9 a-z
-            // 0.2312345121 -> "0.hedfa674338sasfamx" -> 
-            //452384772534
-            const uniqueFileName = Math.random().toString(36).substring(2) + "-" + Date.now() + "-" + fileName
+export const multerUpload = multer({ storage: storage });
 
-            return uniqueFileName
-        }
-    }
-})
 
-export const multerUpload = multer({ storage: storage })
+
+
+
+
+
+// import multer from "multer";
+// import { CloudinaryStorage } from "multer-storage-cloudinary";
+// import { cloudinaryUpload } from "./cloudinary.config";
+
+
+// const storage = new CloudinaryStorage({
+//     cloudinary: cloudinaryUpload,
+//     params: {
+//         public_id: (req, file) => {
+//             // My Special.Image#!@.png => 4545adsfsadf-45324263452-my-image.png
+//             // My Special.Image#!@.png => [My Special, Image#!@, png]
+
+//             const fileName = file.originalname
+//                 .toLowerCase()
+//                 .replace(/\s+/g, "-") // empty space remove replace with dash
+//                 .replace(/\./g, "-")
+//                 // eslint-disable-next-line no-useless-escape
+//                 .replace(/[^a-z0-9\-\.]/g, "") // non alpha numeric - !@#$
+
+//             // const extension = file.originalname.split(".").pop()
+
+//             // binary -> 0,1 hexa decimal -> 0-9 A-F base 36 -> 0-9 a-z
+//             // 0.2312345121 -> "0.hedfa674338sasfamx" -> 
+//             //452384772534
+//             const uniqueFileName = Math.random().toString(36).substring(2) + "-" + Date.now() + "-" + fileName
+
+//             return uniqueFileName
+//         }
+//     }
+// })
+
+// export const multerUpload = multer({ storage: storage })
