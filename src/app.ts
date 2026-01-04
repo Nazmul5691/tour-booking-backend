945dc939ea77e3ad80dba6072a716b90a481a093
Md Nazmul Islam <nazmulislam5691@gmail.com>
2026-01-04 19:56:19 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.use(express.json())
 app.set("trust proxy", 1)
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: envVars.FRONTEND_URL,
+    origin: ['envVars.FRONTEND_URL', 'http://localhost:3000', 'https://tour-booking-frontend-indol.vercel.app'],
     credentials: true
 }));
 
